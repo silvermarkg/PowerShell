b23a95222249372730bc2ee9074192c37793116c
silvermarkg <19527097+silvermarkg@users.noreply.github.com>
2022-01-06 12:37:24 +0000

    PS template upload

---
+++ _PSTemplate.ps1
@@ -0,0 +1,102 @@
+<#
+  .NOTES
+  Author: Mark Goodman
+  Release Notes:
+  Version 1.0: Original published version
+
+  MIT LICENSE
+  
+  Copyright (c) 2021 Microsoft
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+  .SYNOPSIS
+  <Short description>
+
+  .DESCRIPTION
+  <Long description>
+
+  .PARAMETER ParamName
+  <parameter description>
+	
+  .EXAMPLE
+  <ScriptName>.ps1
+	
+	Description
+	-----------
+	<example description>
+#>
+
+#-- Parameters --#
+[cmdletbinding(SupportsShouldProcess=$true,ConfirmImpact='Low')]
+param(
+  # Mandatory parameter
+  [Parameter(Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$True,HelpMessage="",Position=0)]
+    [ValidateNotNullOrEmpty()]
+    [String]$Name,
+
+  # Non-mandatory Parameter
+  [Parameter(Mandatory=$false,HelpMessage="")]
+    [String]$Name2
+)
+
+#-- Script Environment --#
+#Requires -Version 4
+# #Requires -RunAsAdministrator
+Set-StrictMode -Version Latest
+
+#-- Functions --#
+function Do-Something {
+  <#
+  .SYNOPSIS
+  Describe the function here
+  .DESCRIPTION
+  Describe the function in more detail
+  .EXAMPLE
+  Give an example of how to use it
+  .EXAMPLE
+  Give another example of how to use it
+  .PARAMETER computername
+  The computer name to query. Just one.
+  .PARAMETER logname
+  The name of a file to write failed computer names to. Defaults to errors.txt.
+  #>
+
+  [CmdletBinding(SupportsShouldProcess=$true,ConfirmImpact='Low')]
+  param
+  (
+    [Parameter(Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true,HelpMessage="")]
+      [ValidateNotNullOrEmpty()]
+      [String[]]$Name
+  )
+
+  begin
+  {
+    #-- Begin code only runs once --#
+  }
+
+  process
+  {
+    #-- Process block --#
+    write-verbose "Beginning process loop"
+
+    foreach ($Item in $Name) {
+      Write-Verbose "Processing $Item"
+
+      # Following if statement handles the -WhatIf and -Confirm switches
+      if ($PSCmdlet.ShouldProcess($Item)) {
+        # use $Item here
+      }
+    }
+  }
+}
+
+#-- Script variables --#
+# PS v2+ = $scriptDir = split-path -path $MyInvocation.MyCommand.Path -parent
+# PS v4+ = Use $PSScriptRoot for script path
+
+#-- Main code block --#
